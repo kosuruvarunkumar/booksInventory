a576325fa005fa0d5fe462b39179abf365aa6b83
kosuruvarunkumar <kosuruvarunkumar@gmail.com>
2022-03-05 22:51:31 +0530

    BoilerPlate code

---
+++ src/main/java/com/example/booksInventory/BooksInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksInventoryApplication.class, args);
+	}
+
+}
